b42dbeb119216d92adf8ab39fec3d7797e731a4c
Ducky463 <ThePotatoKiddo@gmail.com>
2024-08-30 10:10:32 +0800

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("secretLink").addEventListener("click", function() {
-    window.location.href = "file:///C:/Users/Acer/Desktop/Spinning%20corn/index.html";
+    window.location.href = "https://ducky463.github.io/Spinning-corn/";
 });
